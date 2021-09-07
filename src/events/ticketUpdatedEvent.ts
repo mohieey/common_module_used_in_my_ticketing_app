dcc8f8e45994dbb2081acaa6daca161ce235ea29
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-07 06:40:11 +0200

    updates

---
+++ src/events/ticketUpdatedEvent.ts
@@ -0,0 +1,6 @@
+import { Subjects } from "./subjects";
+
+export interface TicketUpdatedEvent {
+  subject: Subjects.TicketUpdated;
+  data: { id: string; title: string; price: number; userId: string };
+}
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,9 @@ export * from "./middlewares/currentUser";
 export * from "./middlewares/errorHandler";
 export * from "./middlewares/requireAuth";
 export * from "./middlewares/validateRequest";
+
+export * from "./events/baseListener";
+export * from "./events/basePublisher";
+export * from "./events/subjects";
+export * from "./events/ticketCreatedEvent";
+export * from "./events/ticketUpdatedEvent";
